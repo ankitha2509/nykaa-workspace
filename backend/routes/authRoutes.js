5499e1c598b69977d7a87e56469528c59660cf6f
ankitha2509 <ankithagbangera@gmail.com>
2026-04-20 19:06:01 +0530

    added email otp with nodemailer

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const sendEmail = require("../utils/sendEmail");
+const sendEmail = require("../config/sendEmail");
 
 const generateOTP = () => {
   return Math.floor(100000 + Math.random() * 900000).toString();
