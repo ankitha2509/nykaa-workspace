27cef3442d3466a7465a99f96045e451bc653391
ankitha2509 <ankithagbangera@gmail.com>
2026-05-04 17:46:08 +0530

    Code

--- backend/config/sendEmail.js
+++ backend/config/sendEmail.js
@@ -1,10 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4, // 🔥 FORCE IPv4 (VERY IMPORTANT)
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
@@ -12,24 +9,16 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async (to, otp) => {
-  try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to,
-      subject: "Nykaa OTP Verification",
-      html: `
-        <h2>Your OTP Code</h2>
-        <h1>${otp}</h1>
-        <p>This OTP is valid for 5 minutes.</p>
-      `
-    });
-
-    console.log("✅ Email sent");
-
-  } catch (error) {
-    console.log("❌ Email error:", error.message);
-  }
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to,
+    subject: "Nykaa OTP Verification",
+    html: `
+      <h2>Your OTP Code</h2>
+      <h1>${otp}</h1>
+      <p>This OTP is valid for 5 minutes.</p>
+    `
+  });
 };
 
 module.exports = sendEmail;
-
